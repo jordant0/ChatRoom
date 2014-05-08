98fec90a2c291c242383caf88e1d56c9252da920
Rob Mack <robm@spiceworks.com>
2014-05-07 22:40:25 -0500

    scroll the transcript to the bottom on page load

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+// scroll the chat transcript to the top
+$( document ).ready(function() {
+  $('.message-window').scrollTop($('.message-window')[0].scrollHeight);
+});
